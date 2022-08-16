e7933b519128a9a7f0cdb40e46cd012bbedf701a
ahmad940 <ahmadmuhammadmak5@gmail.com>
2022-08-16 13:41:24 +0100

    fixed error

--- util/struct.validator.go
+++ util/struct.validator.go
@@ -4,7 +4,7 @@ import (
 	"strings"
 
 	"github.com/go-playground/validator/v10"
-	"github.com/medical-identification/model"
+	"github.com/medical-identification/go-std/model"
 )
 
 var validate = validator.New()
