c3935c08956b122c31b3ad7d9a9894642b479b5c
Ahmad940 <Ahmadmuhammadmak5@gmail.com>
2023-01-26 15:48:44 +0100

    added validation for null types

--- util/struct.validator.go
+++ util/struct.validator.go
@@ -1,13 +1,21 @@
 package util
 
 import (
+	"database/sql"
+	"database/sql/driver"
+	"reflect"
 	"strings"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
 	"github.com/medical-identification/go-std/model"
 )
 
 var validate = validator.New()
+
+func init() {
+	validate.RegisterCustomTypeFunc(ValidateValuer, sql.NullString{}, sql.NullInt64{}, sql.NullBool{}, sql.NullFloat64{}, sql.NullInt16{}, sql.NullTime{}, sql.NullByte{}, sql.NullInt32{}, uuid.NullUUID{})
+}
 
 func ValidateStruct(obj interface{}) model.ErrorValidationResponse {
 	var errors model.ErrorValidationResponse = model.ErrorValidationResponse{}
@@ -24,4 +32,19 @@ func ValidateStruct(obj interface{}) model.ErrorValidationResponse {
 		}
 	}
 	return errors
+}
+
+// ValidateValuer implements validator.CustomTypeFunc
+func ValidateValuer(field reflect.Value) interface{} {
+
+	if valuer, ok := field.Interface().(driver.Valuer); ok {
+
+		val, err := valuer.Value()
+		if err == nil {
+			return val
+		}
+		// handle the error how you want
+	}
+
+	return nil
 }
